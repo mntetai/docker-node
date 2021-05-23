a60d276483be86b23ec2fef281065e706479a84a
mntetai <mntetai@gmail.com>
2021-05-23 16:19:25 +0300

    Revert "Revert "initial commit""
    This reverts commit 8b8337ab441b270ea62e588d2b9e734fc3ef0700.

---
+++ ex1/routes/users.js
@@ -0,0 +1,54 @@
+var express = require('express');
+var router = express.Router();
+var db = require('../database');
+
+router.get("/all", function(req, res) {
+    db.Users.findAll()
+        .then( users => {
+            res.status(200).send(JSON.stringify(users));
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+router.get("/:id", function(req, res) {
+    db.Users.findByPk(req.params.id)
+        .then( user => {
+            res.status(200).send(JSON.stringify(user));
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+router.put("/", function(req, res) {
+    db.Users.create({
+        Name: req.body.name,
+        Email : req.body.email,
+        Id: req.body.id,
+        Password : req.body.password
+        })
+        .then( user => {
+            res.status(200).send(JSON.stringify(user));
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+router.delete("/:id", function(req, res) {
+    db.Users.destroy({
+        where: {
+            id: req.params.id
+        }
+        })
+        .then( () => {
+            res.status(200).send();
+        })
+        .catch( err => {
+            res.status(500).send(JSON.stringify(err));
+        });
+});
+
+module.exports = router;
